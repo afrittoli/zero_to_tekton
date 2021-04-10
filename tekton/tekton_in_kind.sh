2ea23be53bb7dc1e2b6f213bed243e051d84b51b
Andrea Frittoli <andrea.frittoli@gmail.com>
2021-04-10 17:16:35 +0100

    fix local registry setup
    Signed-off-by: Andrea Frittoli <andrea.frittoli@gmail.com>

--- tekton/tekton_in_kind.sh
+++ tekton/tekton_in_kind.sh
@@ -102,8 +102,6 @@ containerdConfigPatches:
 - |-
   [plugins."io.containerd.grpc.v1.cri".registry.mirrors."localhost:${reg_port}"]
     endpoint = ["http://${reg_name}:${reg_port}"]
-  [plugins."io.containerd.grpc.v1.cri".registry.mirrors."${reg_name}:${reg_port}"]
-    endpoint = ["http://${reg_name}:${reg_port}"]
 EOF
 fi
 
